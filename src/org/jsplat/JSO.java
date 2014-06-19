14974ba41bc61fcca498530b400680eddc0a77fb
Joe Dailey <josephldailey@gmail.com>
2014-06-19 15:42:01 -0500

    splitting the object at the current level works via splitTop()

--- src/org/jsplat/JSO.java
+++ src/org/jsplat/JSO.java
@@ -1,7 +1,10 @@
 package org.jsplat;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Stack;
 
 public class JSO {
 	public static final int TYPE_OBJECT = 0;
@@ -11,6 +14,12 @@ public class JSO {
 	public static final int TYPE_FUNCTION = 4;
 	public static final int TYPE_REFERENCE = 5;
 	public static final int TYPE_STRING = 6;
+	
+	private static final Map<Character, Character> enclosers = new HashMap<Character, Character>(){{
+		put('{', '}');
+		put('[', ']');
+		put('\"', '\"');
+	}};
 
 	private JSO root;
 	private JSO parent;
@@ -31,23 +40,34 @@ public class JSO {
 	public JSO(String json) throws malformedJSON{
 		root = this;
 		json = json.replace(" ", "").replace("\t", "").replace("\n", "");
-		
+
 		if(json.charAt(0) != '{') throw new malformedJSON(0, "{", json.charAt(0));
+		if(json.charAt(json.length()-1) != '}') throw new malformedJSON(json.length()-1, "}", json.charAt(json.length()-1));
 		
+		json = json.substring(1, json.length()-1);
+		
+		String[] split = json.split(",");
+		for(String s : split){
+			System.out.println(s);
+		}
 		type = TYPE_OBJECT;
 		data = new HashMap<String, JSO>();
 		Map<String, JSO> thisObject = (Map<String, JSO>)data;
 		
+		for(String line : split){
+			System.out.println(line);
+			
+		}
+		
 		for(int i = 0; i < json.length(); i++){
 			switch(json.charAt(i)){
 				case '"':
-					System.out.println(json.charAt(i));//TODO remove;
 
 					if(!can_next) throw new malformedJSON(i, ",", json.charAt(i));
 					i++;//past open quote
 					String tag = json.substring(i, json.indexOf('"', i));
 					i += tag.length() + 1;//past tag and close quote
-					System.out.println(tag);//TODO remove;
+
 					if(json.charAt(i)!=':') throw new malformedJSON(i, ":", json.charAt(i));
 					i++;//past colon
 					JSO newJSO = new JSO(root, parent);
@@ -67,11 +87,55 @@ public class JSO {
 			}
 		}
 	}
+	
+	public static String[] splitTop(String json){
+		List<String> props = new ArrayList<String>();
+		Stack<Character> scope = new Stack<Character>();
+		json = clean(json);
+		if(
+			(json.charAt(0) == '{' && json.charAt(json.length()-1) == '}')
+			||
+			(json.charAt(0) == '[' && json.charAt(json.length()-1) == ']')
+		) json = json.substring(1, json.length()-1);
+		int quotes = 0;
+		for(int i = 0; i < json.length(); i++){
+			if(i+1 == json.length()){
+				if(scope.empty() || json.charAt(i) == scope.peek()){
+					props.add(json);
+					break;
+				}
+			}
+			if(json.charAt(i) == '\"')
+				quotes++;
+			// if the char is an encloser and the top of the stack is not a quote the push
+			if( JSO.enclosers.containsKey(json.charAt(i)) && !(!scope.empty() && scope.peek()=='"') )
+				scope.push( JSO.enclosers.get(json.charAt(i)) );
+			
+			else if( JSO.enclosers.containsValue(json.charAt(i)) && scope.peek() == json.charAt(i) )
+				scope.pop();
+				
+			else if( json.charAt(i) == ',' )
+				if(scope.empty()){
+					props.add(json.substring(0, i));
+					json = json.substring(i+1, json.length());
+					i = -1;
+				}
+				
+		}
+		
+		return props.toArray(new String[0]);
+	}
+	private static String clean(String json){
+		return json.replace(" ", "").replace("\t", "").replace("\n", "");
+	}
+	
 	private int newJSO(JSO newJSO, JSO root, JSO parent, String json, int i){
 		for(;i < json.length(); i++){
 			switch(json.charAt(i)){
 				case '{':
 					newJSO.type = TYPE_OBJECT;
+					i++;
+					newJSO.data = new HashMap<String, JSO>();
 				break;
 				case '"':
 					newJSO.type = TYPE_STRING;
--- src/org/jsplat/test.java
+++ src/org/jsplat/test.java
@@ -3,16 +3,23 @@ package org.jsplat;
 public class test {
 
 	public static void main(String[] args) {
-		String json = "{\n\"glossary\": \"title\",\"asdf\": \"qwer\"\n}";
+		String json = "{\n\"glossary\": \"title\",\"asdf\": \",\"\n}";
 //		String jsonBlacked = {"glossary":"title"};
-		JSO JSON_OBJECT = null;
-		try {
-			JSON_OBJECT = new JSO(json);
-		} catch (malformedJSON e) {
-			System.out.println(e.getMessage());
-			e.printStackTrace();
-		}
-		System.out.println("wat");
+		
+		String[] split = JSO.splitTop(json);
+		for(String s: split)
+			System.out.println(s);
+		
+		
+		
+//		JSO JSON_OBJECT = null;
+//		try {
+//			JSON_OBJECT = new JSO(json);
+//		} catch (malformedJSON e) {
+//			System.out.println(e.getMessage());
+//			e.printStackTrace();
+//		}
+//		System.out.println("wat");
 	}
 
 }
