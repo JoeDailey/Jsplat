475614cbcb29faca5faab991607a64f7ef15f963
Joe Dailey <josephldailey@gmail.com>
2014-06-19 18:11:54 -0500

    JSON parsing complete and working as expected. cicular does not

--- src/org/jsplat/JSO.java
+++ src/org/jsplat/JSO.java
@@ -4,16 +4,19 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 import java.util.Stack;
 
 public class JSO {
 	public static final int TYPE_OBJECT = 0;
 	public static final int TYPE_ARRAY = 1;
-	public static final int TYPE_DOUBLE = 2;
+	public static final int TYPE_NUMBER = 2;
 	public static final int TYPE_BOOLEAN = 3;
 	public static final int TYPE_FUNCTION = 4;
-	public static final int TYPE_REFERENCE = 5;
+//	public static final int TYPE_REFERENCE = 5;
 	public static final int TYPE_STRING = 6;
+	public static final int TYPE_UNDEFINED = 7;
+	public static final int TYPE_NULL = 8;
 	
 	private static final Map<Character, Character> enclosers = new HashMap<Character, Character>(){{
 		put('{', '}');
@@ -38,56 +41,120 @@ public class JSO {
 	
 	@SuppressWarnings("unchecked")
 	public JSO(String json) throws malformedJSON{
-		root = this;
-		json = json.replace(" ", "").replace("\t", "").replace("\n", "");
-
-		if(json.charAt(0) != '{') throw new malformedJSON(0, "{", json.charAt(0));
-		if(json.charAt(json.length()-1) != '}') throw new malformedJSON(json.length()-1, "}", json.charAt(json.length()-1));
+		this(TYPE_OBJECT, null, json);
+		this.root = this;
+	}
+	@SuppressWarnings("unchecked")
+	public JSO(int TYPE, JSO root, String json) throws malformedJSON{
+		this.root = root;
+		this.type = TYPE_OBJECT;
 		
-		json = json.substring(1, json.length()-1);
-		
-		String[] split = json.split(",");
-		for(String s : split){
-			System.out.println(s);
-		}
-		type = TYPE_OBJECT;
-		data = new HashMap<String, JSO>();
-		Map<String, JSO> thisObject = (Map<String, JSO>)data;
-		
-		for(String line : split){
-			System.out.println(line);
-			
-		}
-		
-		for(int i = 0; i < json.length(); i++){
-			switch(json.charAt(i)){
-				case '"':
+		switch(TYPE){
+			case TYPE_OBJECT:
+				this.data = new HashMap<String, JSO>();
+				
+				Map<String, JSO> me = (Map<String, JSO>)this.data;
+				
+				for(String prop : splitTop(json)){
+					String[] part = prop.split(":", 2);
+					if(part.length!=2) throw new malformedJSON("tag and property separated by a :", prop);
+					if(part[0].charAt(0) == '"') part[0] = part[0].substring(1, part[0].length());
+					if(part[0].charAt(part[0].length()-1) == '"') part[0] = part[0].substring(0, part[0].length()-1);
 
-					if(!can_next) throw new malformedJSON(i, ",", json.charAt(i));
-					i++;//past open quote
-					String tag = json.substring(i, json.indexOf('"', i));
-					i += tag.length() + 1;//past tag and close quote
-
-					if(json.charAt(i)!=':') throw new malformedJSON(i, ":", json.charAt(i));
-					i++;//past colon
-					JSO newJSO = new JSO(root, parent);
-					i = newJSO(newJSO, root, this, json, i);
-					
-					
-					thisObject.put(tag, newJSO);
-	
-				break;
-				case ',':
-					can_next = true;
-					i++;
-				break;
-
-				case '}':
-					return;
-			}
+					switch(part[1].charAt(0)){
+						case '\"':	//is String
+							me.put(part[0], new JSO(TYPE_STRING, root, part[1]));
+						break;
+						case '{':	//is object
+							me.put(part[0], new JSO(TYPE_OBJECT, root, part[1]));
+						break;
+						case '[':	//is array	
+							me.put(part[0], new JSO(TYPE_ARRAY, root, part[1]));
+						break;
+						default:
+							if( part[1].equals("undefined") || part[1].equals("UNDEFINED") )//is undefined
+								me.put(part[0], new JSO(TYPE_UNDEFINED, root, part[1]));
+							else if( part[1].equals("null") || part[1].equals("NULL") )	//is null
+								me.put(part[0], new JSO(TYPE_NULL, root, part[1]));
+							else if( part[1].equals("true") || part[1].equals("TRUE")
+									||
+									part[1].equals("false") || part[1].equals("FALSE") )//is bool
+								me.put(part[0], new JSO(TYPE_BOOLEAN, root, part[1]));
+							else if( part[1].contains("function") )	//is function
+								me.put(part[0], new JSO(TYPE_FUNCTION, root, part[1]));
+							else{
+								try{  
+								    Double.parseDouble(part[1]);
+								    me.put(part[0], new JSO(TYPE_NUMBER, root, part[1]));
+								}catch(Exception e){
+									throw new malformedJSON("type of STRING, NUMBER, NULL, TRUE, FALSE, OBJECT, ARRAY, or UNDEFINED", part[1]);  
+								}
+							}
+						break;
+					}
+				}
+			break;
+			case TYPE_ARRAY:
+				this.data = new HashMap<String, JSO>();
+				
+				Map<String, JSO> meArr = (Map<String, JSO>)this.data;
+				
+				int propCount = 0;
+				
+				for(String prop : splitTop(json)){
+					switch(prop.charAt(0)){
+						case '\"':	//is String
+							meArr.put(""+propCount, new JSO(TYPE_STRING, root, prop));
+						break;
+						case '{':	//is object
+							meArr.put(""+propCount, new JSO(TYPE_OBJECT, root, prop));
+						break;
+						case '[':	//is array	
+							meArr.put(""+propCount, new JSO(TYPE_ARRAY, root, prop));
+						break;
+						default:
+							if( prop.equals("undefined") || prop.equals("UNDEFINED") )//is undefined
+								meArr.put(""+propCount, new JSO(TYPE_UNDEFINED, root, prop));
+							else if( prop.equals("null") || prop.equals("NULL") )	//is null
+								meArr.put(""+propCount, new JSO(TYPE_NULL, root, prop));
+							else if( prop.equals("true") || prop.equals("TRUE")
+									||
+									prop.equals("false") || prop.equals("FALSE") )//is bool
+								meArr.put(""+propCount, new JSO(TYPE_BOOLEAN, root, prop));
+							else if( prop.contains("function") )	//is function
+								meArr.put(""+propCount, new JSO(TYPE_FUNCTION, root, prop));
+							else{
+								try{  
+								    Double.parseDouble(prop);
+								    meArr.put(""+propCount, new JSO(TYPE_NUMBER, root, prop));
+								}catch(Exception e){
+									throw new malformedJSON("type of STRING, NUMBER, NULL, TRUE, FALSE, OBJECT, ARRAY, or UNDEFINED", prop);  
+								}
+							}
+						break;
+					}
+				propCount++;
+				}
+			break;
+			case TYPE_STRING:
+				this.data = json.substring(1, json.length()-1);
+			break;
+			case TYPE_NUMBER:
+				this.data = Double.parseDouble(json.substring(1, json.length()));
+			break;
+			case TYPE_BOOLEAN:
+				if( json.toLowerCase().equals("true") )
+					data = true;
+				else
+					data = false;
+			break;
+			default:
+				this.data = json;
+			break;
 		}
 	}
 	
+
 	public static String[] splitTop(String json){
 		List<String> props = new ArrayList<String>();
 		Stack<Character> scope = new Stack<Character>();
@@ -97,7 +164,7 @@ public class JSO {
 			||
 			(json.charAt(0) == '[' && json.charAt(json.length()-1) == ']')
 		) json = json.substring(1, json.length()-1);
-		int quotes = 0;
+
 		for(int i = 0; i < json.length(); i++){
 			if(i+1 == json.length()){
 				if(scope.empty() || json.charAt(i) == scope.peek()){
@@ -105,17 +172,20 @@ public class JSO {
 					break;
 				}
 			}
-			if(json.charAt(i) == '\"')
-				quotes++;
 			// if the char is an encloser and the top of the stack is not a quote the push
 			if( JSO.enclosers.containsKey(json.charAt(i)) && !(!scope.empty() && scope.peek()=='"') )
+				//push the counter encloser
 				scope.push( JSO.enclosers.get(json.charAt(i)) );
 			
+			//if the char is a counter encloser and equals the top of the stack
 			else if( JSO.enclosers.containsValue(json.charAt(i)) && scope.peek() == json.charAt(i) )
 				scope.pop();
-				
+			
+			//if the char is a prop separator
 			else if( json.charAt(i) == ',' )
+				//and the scope is empty (at top level)
 				if(scope.empty()){
+					//add the prop to the list and shorten json/reset count for pre-increment 
 					props.add(json.substring(0, i));
 					json = json.substring(i+1, json.length());
 					i = -1;
@@ -126,7 +196,12 @@ public class JSO {
 		return props.toArray(new String[0]);
 	}
 	private static String clean(String json){
-		return json.replace(" ", "").replace("\t", "").replace("\n", "");
+		Scanner s = new Scanner(json);
+		json = "";
+		while(s.hasNext()){
+			json += s.findInLine("[^\"\\s]+|\"(\\\\.|[^\\\\\"])*\"");
+		}
+		return json.replace("\t", "").replace("\n", "");
 	}
 	
 	private int newJSO(JSO newJSO, JSO root, JSO parent, String json, int i){
@@ -158,7 +233,12 @@ public class JSO {
 	
 	
 	
-//	public Object get(String node);
+//	public Object get(String node){
+//		int index = 0;
+//		String[] route = node.split("\\.");
+//		if(index route.length )
+//		return null;
+//	}
 //	public  void set(String node, String Value);
 //	public JSO traverse(String node);
 //	public JSO parent();
@@ -181,17 +261,19 @@ public class JSO {
 
 @SuppressWarnings("serial")
 class malformedJSON extends Exception{
-	private final int ln;
 	private final String expected;
-	private final char instead;
-	malformedJSON(int ln, String expected, char instead){
-		this.ln = ln;
+	private final String instead;
+	malformedJSON(String expected, String instead){
 		this.expected = expected;
 		this.instead = instead;
 	}
+	malformedJSON(String expected, char instead){
+		this.expected = expected;
+		this.instead = instead + "";
+	}
 	@Override
 	public String getMessage() {
-		return "Expected \"" + expected + "\", instead found \"" + instead + "\"" + " at index " + ln;
+		return "Expected " + expected + ", instead found " + instead;
 	}
 	
 }
--- src/org/jsplat/test.java
+++ src/org/jsplat/test.java
@@ -3,8 +3,11 @@ package org.jsplat;
 public class test {
 
 	public static void main(String[] args) {
-		String json = "{\n\"glossary\": \"title\",\"asdf\": \",\"\n}";
+//		String json = "{\n\"glossary\": \"title\",\"asdf\": \",\"\n}";
+//		String json = "{\n\"glossary\": {},\"asdf\": \",\"\n}";
+		String json = "{   \"glossary\":{\"title\":\"example glossary\",\"GlossDiv\":{\"title\":\"S\",\"GlossList\":{\"GlossEntry\":{\"ID\":\"SGML\",\"SortAs\":\"SGML\",\"GlossTerm\":\"Standard Generalized Markup Language\",\"Acronym\":\"SGML\",\"Abbrev\":\"ISO 8879:1986\",\"GlossDef\":{\"para\":\"A meta-markup language, used to create markup languages such as DocBook.\",\"GlossSeeAlso\":[\"GML\",\"XML\"]},\"GlossSee\":\"markup\"}}}}}";
 //		String jsonBlacked = {"glossary":"title"};
+//		String json = "\"asdfasdf\",\"asdfasdf\",\"asdfasdf\"";
 		
 		String[] split = JSO.splitTop(json);
 		for(String s: split)
@@ -12,14 +15,16 @@ public class test {
 		
 		
 		
-//		JSO JSON_OBJECT = null;
-//		try {
-//			JSON_OBJECT = new JSO(json);
-//		} catch (malformedJSON e) {
-//			System.out.println(e.getMessage());
-//			e.printStackTrace();
-//		}
-//		System.out.println("wat");
+		JSO JSON_OBJECT = null;
+		try {
+			JSON_OBJECT = new JSO(json);
+		} catch (malformedJSON e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+		
+//		JSON_OBJECT.get("glossary.title");
+		
 	}
 
 }
